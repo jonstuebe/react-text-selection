803150b1edfc626b601d1cc5034d1f12d7c89a05
Jonathan Stuebe <jstuebe@gmail.com>
2017-12-20 09:39:02 -0700

    added rollup for browser and esnext builds

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
+import autoExternal from "rollup-plugin-auto-external";
+import pkg from "./package.json";
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: pkg.source,
+    output: {
+      file: pkg.browser,
+      format: "umd"
+    },
+    name: pkg.umdName,
+    plugins: [
+      babel({ exclude: "node_modules/**" }),
+      resolve(),
+      commonjs(),
+      autoExternal()
+    ],
+    globals: {
+      react: "react"
+    }
+  },
+  {
+    input: pkg.source,
+    output: [
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" }
+    ],
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      autoExternal()
+    ]
+  }
+];
